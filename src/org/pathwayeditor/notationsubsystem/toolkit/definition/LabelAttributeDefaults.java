0c9903964bcf4d43d12fe2337d058fcad8297363
Stuart Moodie <stuart.moodie@eightpillars.uk.com>
2011-04-20 17:37:47 +0000

    Added Apache licenses to the code.

--- src/org/pathwayeditor/notationsubsystem/toolkit/definition/LabelAttributeDefaults.java
+++ src/org/pathwayeditor/notationsubsystem/toolkit/definition/LabelAttributeDefaults.java
@@ -1,4 +1,24 @@
+/*
+  Licensed to the Court of the University of Edinburgh (UofE) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The UofE licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+*/
 package org.pathwayeditor.notationsubsystem.toolkit.definition;
+
+import java.text.Format;
 
 import org.pathwayeditor.businessobjects.drawingprimitives.attributes.LabelLocationPolicy;
 import org.pathwayeditor.businessobjects.drawingprimitives.attributes.LineStyle;
@@ -15,6 +35,7 @@ public class LabelAttributeDefaults implements ILabelAttributeDefaults {
 	private boolean noFill = true;
 	private boolean noBorder = true;
 	private Dimension minimumSize;
+	private Format displayFormat = null;
 
 	public LabelAttributeDefaults() {
 	}
@@ -89,6 +110,15 @@ public class LabelAttributeDefaults implements ILabelAttributeDefaults {
 	@Override
 	public boolean hasNoFill() {
 		return this.noFill;
+	}
+
+	public void setDisplayFormat(Format displayFormat){
+		this.displayFormat = displayFormat;
+	}
+	
+	@Override
+	public Format getDisplayFormat() {
+		return this.displayFormat;
 	}
 
 }
